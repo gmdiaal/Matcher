87e6a08c9a9bc7cc65f22face0cd89ef07bc79e1
gmdiaal <gmdiaal@gmail.com>
2023-05-18 10:40:16 +0900

    home

--- src/main/java/com/iu/base/test/TestController.java
+++ src/main/java/com/iu/base/test/TestController.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class TestController {
 
 	@GetMapping("/")
